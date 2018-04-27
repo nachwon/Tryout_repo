31af0f1a984cf3e8dd4867a1e2abf87b35fa7755
nachwon <nachwon@gmail.com>
2018-04-27 22:59:38 +0900

    Refactor PostListView to retrieve cached data from redis

--- practice/post/views.py
+++ practice/post/views.py
@@ -1,3 +1,4 @@
+from django.core.cache import cache
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.generic import ListView
@@ -9,8 +10,12 @@ class PostListView(ListView):
     model = Post
 
     def get(self, request, *args, **kwargs):
-        posts = Post.objects.all().values('id', 'title', 'content')
-        context = {}
-        for i in posts:
-            context[f'post_{i["id"]}'] = i
+        context = cache.get('posts')
+
+        if not context:
+            posts = Post.objects.all().values('id', 'title', 'content')
+            context = {}
+            for i in posts:
+                context[f'post_{i["id"]}'] = i
+            cache.set('posts', context)
         return JsonResponse(context)
