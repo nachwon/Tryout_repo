f0f41ce302987b15705df9eb80f2186d70b85f39
nachwon <nachwon@gmail.com>
2018-04-27 20:33:33 +0900

    Add post list view

--- practice/post/views.py
+++ practice/post/views.py
@@ -1,3 +1,13 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from post.models import Post
 
 
+class PostListView(ListView):
+    model = Post
+
+    def get(self, request, *args, **kwargs):
+        posts = Post.objects.all().values('id', 'title', 'content')
+        return JsonResponse(list(posts), safe=False)
