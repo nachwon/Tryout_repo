8f57f0fa9e0e43d3fef254207027d8969b50ee0c
nachwon <nachwon@gmail.com>
2018-04-27 20:36:03 +0900

    Add url to post list

--- practice/config/urls.py
+++ practice/config/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.urls import path
+
+from post.views import PostListView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    path('posts/', PostListView.as_view()),
 ]
