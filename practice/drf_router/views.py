a8eacbd5b20e36bebf5f1b66317f58262e1c106f
nachwon <nachwon@gmail.com>
2018-05-06 22:27:14 +0900

    <feat>(drf_router): Add PostViewSet and a custom action method

--- practice/drf_router/views.py
+++ practice/drf_router/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from drf_router.serializer import PostSerializer
+from mysql_redis.models import Post
+
+
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    @action(methods=['post'], detail=True)
+    def custom_action(self, request, *args, **kwargs):
+        context = {
+            'detail': 'this is a custom action',
+            'request': f'{request}',
+            'args': args,
+            'kwargs': kwargs
+        }
+        return Response(context, status=200)
